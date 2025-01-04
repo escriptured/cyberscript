2adf10ec8cec6d10c6c1c011e02ef208975d8f3c
escriptured <iamandrewj@outlook.com>
2025-01-04 17:46:37 +0000

    Update ugui.php
    #clear

--- ugui.php
+++ ugui.php
@@ -1,27 +1,3 @@
 <?php
 
-$o=[];
-
-$o[]='<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>GUID</title>
-        <link rel="stylesheet" href="style.css" />
-    </head>
-    <body>
-        <div class="container">
-            <div class="header">
-                <p class="quote" id="quote">Generate a UUID</p>
-                <button class="btn" id="btn">Generate</button>
-            </div>
-        </div>
-        <!-- Javascript includes -->
-        <script src="index.js"></script>
-    </body>
-</html>';
-
-echo join('',$o);
 ?>
