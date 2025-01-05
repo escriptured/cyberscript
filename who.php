06f3a70d608d60cd9e4cfb3b843ef6eaf40b98f6
escriptured <iamandrewj@outlook.com>
2025-01-05 14:31:07 +0000

    Create who.php
    escripture cyber - unique universal code, enables your applications, functions, events and services, to have world no co-incident identifier.

---
+++ who.php
@@ -0,0 +1,23 @@
+<?php
+$is_exist=false;
+$w=[];
+
+
+function universal_auth_db($a) {
+  $pass=false
+
+  $pass=true
+  return $pass;
+}
+while ($is_exist) {
+$w[]=series(128);
+$w[]=series(32);
+$w[]=series(32);
+$w[]=series(32);
+$exist=universal_auth_db($w);
+}
+
+echo " # ".json_encode($w);
+?>
+
+
