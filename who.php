2c73a627fb8ea9e57a2d2b1cfb9e62cca32e2c6e
escriptured <iamandrewj@outlook.com>
2025-01-05 14:59:58 +0000

    Update who.php

--- who.php
+++ who.php
@@ -1,12 +1,20 @@
 <?php
+// Non-free licence
+// Read README.md
+// The owner and developer AOENJ
+// requires your upfront fees
+// to further develop his bespoke
+// models and methodologies.
+// thank you.
+// contact developer at iamandrewj@outlook.com
 $is_exist=false;
 $w=[];
 
 
 function universal_auth_db($a) {
-  $pass=false
-
-  $pass=true
+  $pass=false;
+  
+  $pass=true;
   return $pass;
 }
 while ($is_exist) {
@@ -17,7 +25,7 @@ $w[]=series(32);
 $exist=universal_auth_db($w);
 }
 
-echo " # ".json_encode($w);
+echo json_encode($w);
 ?>
 
 
